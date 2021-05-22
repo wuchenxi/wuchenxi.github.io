1d2946085095e02efbe73386283bf807d0be1987
Chenxi Wu <cw538@cornell.edu>
2021-05-22 16:32:18 -0400

    Update lemmy-ui.js

--- lemmy-ui.js
+++ lemmy-ui.js
@@ -1,2 +1,3 @@
 document.getElementByID("tweet").onclick=function(){window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.title) + ':%20 ' + encodeURIComponent(document.URL));return false;};
 document.getElementByID("fb-share").onclick=function(){window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL) + '&t=' + encodeURIComponent(document.URL)); return false;};
+alert("hi");
